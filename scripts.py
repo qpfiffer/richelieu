35d86d49802f99445b0798bb8c1c255afa6cfdbc
Hamcha <zikyky@gmail.com>
2014-04-25 15:33:49 +0200

    Tweaks to the commit file

--- scripts.py
+++ scripts.py
@@ -16,10 +16,10 @@ def Oleg(owner, repo):
 		# Run ccpcheck
 		print "Running ccpcheck.."
 		cppres = Popen(["cppcheck","-I '"+owner+"/"+repo+"/include' ",owner+"/"+repo+"/src"], stdout=PIPE, stderr=PIPE).stderr.read()
-		commitlog = "COMMIT %s - %s\r\n%s (%s <%s>)\r\n\r\n" % (commit["id"], commit["timestamp"], commit["message"], commit["author"]["name"], commit["author"]["email"])
+		commitlog = "Commit %s - %s\r\n%s (%s <%s>)\r\n\r\n" % (commit["id"], commit["timestamp"], commit["message"], commit["author"]["name"], commit["author"]["email"])
 		# Write to file
 		with open(owner+"."+repo+"."+branch.replace("/",".")+".log", "w") as logfile:
-			logfile.write(commitlog+cppres)
+			logfile.write(commitlog+"CPPCHECK RESULT\r\n\r\n"+cppres)
 		print "Logfile written!"
 		return "All okay!"
 	return process
