6304526d5a8e97e10eb1eafd1f2c7293f2291c9f
Hamcha <zikyky@gmail.com>
2014-04-25 15:17:06 +0200

    Writing to file

--- scripts.py
+++ scripts.py
@@ -4,7 +4,7 @@ from subprocess import Popen, PIPE
 bind = {}
 
 def Oleg(owner, repo):
-	def process(branch):
+	def process(branch, commit):
 		# Select git dir
 		git = Repo(owner+"/"+repo).git
 		# Switch to branch
@@ -16,8 +16,12 @@ def Oleg(owner, repo):
 		# Run ccpcheck
 		print "Running ccpcheck.."
 		cppres = Popen(["cppcheck","-I "+owner+"/"+repo+"/include ",owner+"/"+repo+"/src"], stdout=PIPE).stdout.read()
-		print cppres
-
+		commitlog = "COMMIT %s - %s\r\n%s (%s <%s>)\r\n\r\n" % commit["id"], commit["timestamp"], commit["message"], commit["author"]["name"], commit["author"]]["email"]
+		# Write to file
+		with open(owner+"."+repo+"."+branch+".log", "w") as logfile:
+			logfile.write(commitlog+cppres)
+		print "Logfile written!"
+		return "All okay!"
 	return process
 
 bind["Hamcha/OlegDB"] = Oleg("Hamcha", "OlegDB")
--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 import web
 import json
+import os.path
 
 # Import all scripts
 import scripts
 
 urls = (
     '/richelieu/([^/]*)/([^/]*)', 'push'
+    '/richelieu/([^/]*)/([^/]*)/([^/]*)', 'get'
 )
 
 app = web.application(urls, globals())
@@ -19,7 +21,18 @@ class push:
         print "[%s/%s] New commit on %s by %s <%s>" % (owner, repo, branch, push["pusher"]["name"], push["pusher"]["email"])
         # Do we have a script ready?
         if owner+"/"+repo in scripts.bind:
-            scripts.bind[owner+"/"+repo](branch)
+            scripts.bind[owner+"/"+repo](branch, push["head_commit"])
+
+class get:
+    def GET(self, owner, repo, branch):
+        fname = owner+"."+repo+"."+branch+".log"
+        # Check if logfile exists
+        if os.path.isfile(fname):
+            web.header('Content-Type', 'text/plain')
+            with open(fname, "r") as logfile:
+                return f.read()
+        else:
+            return web.notfound()
 
 if __name__ == "__main__":
     web.httpserver.runsimple(app.wsgifunc(), ("0.0.0.0", 4567))
